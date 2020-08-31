361cdbf181cc2752e7052d0cc04b78f0a0186295
Chris Kay <ckay84@protonmail.com>
2020-08-30 23:20:38 -0400

    Add card model and belongs_to relationship with collection

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_025645) do
+ActiveRecord::Schema.define(version: 2020_08_31_031037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "question"
+    t.string "answer"
+    t.bigint "collection_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["collection_id"], name: "index_cards_on_collection_id"
+  end
 
   create_table "collections", force: :cascade do |t|
     t.string "name"
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_08_31_025645) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "cards", "collections"
 end
