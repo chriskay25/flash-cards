1d27bbc0b7b2d4a70e434e1a4a98457cac1e75f8
Chris Kay <ckay84@protonmail.com>
2020-09-01 19:09:53 -0400

    Index & create actions in collections_controller, and resources

--- backend/app/controllers/api/v1/collections_controller.rb
+++ backend/app/controllers/api/v1/collections_controller.rb
@@ -1,3 +1,22 @@
 class Api::V1::CollectionsController < ApplicationController
+  
+  def index
+    collections = Collection.all
+    render json: CollectionSerializer.new(collections)
+  end 
 
+  def create
+    collection = Collection.new(collection_params)
+    if collection.save
+      render json: collection, status: :accepted
+    else
+      render json: {errors: collection.errors.full_messages}, status: :unprocessable_entity
+    end 
+  end 
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:name)
+  end 
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cards, only: [:index, :create]
+      resources :collections, only: [:index, :create]
     end
   end 
 
