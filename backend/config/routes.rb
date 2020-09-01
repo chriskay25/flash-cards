36f00d6a435b7d02b2fb4561c090adc0a7df91dd
Chris Kay <ckay84@protonmail.com>
2020-09-01 02:38:29 -0400

    Include create resource for Cards

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :cards, only: [:index]
+      resources :cards, only: [:index, :create]
     end
   end 
 
