9480c6bfe62ba3ba23021ab16f1ff06b95950b32
Chris Kay <ckay84@protonmail.com>
2020-09-02 12:33:24 -0400

    Implement get fetch to add Cards to DOM

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,25 @@
 const test = "Is it working?"
+const endPoint = "http://localhost:3000/api/v1/cards"
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Dom is loaded!")
+  getCards()
+})
+
+function getCards() {
+  fetch(endPoint)
+  .then(resp => resp.json())
+  .then(cards => {
+    console.log(cards)
+    cards.data.forEach(card => {
+      const cardMarkup = `
+        <div data-id=${card.id}>
+          <p>Question: ${card.attributes.question}</p>
+          <p>Answer: ${card.attributes.answer}</p>
+        </div>
+      `;
+      document.querySelector('#card-container').innerHTML += cardMarkup
+
+    })
+  })
+}
