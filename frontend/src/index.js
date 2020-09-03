80d6cf1cf3b31ce86a351988cf6c4a2a12b4570c
Chris Kay <ckay84@protonmail.com>
2020-09-03 02:09:42 -0400

    Functions to get form input

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
-const test = "Is it working?"
 const endPoint = "http://localhost:3000/api/v1/cards"
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log("Dom is loaded!")
   getCards()
+
+  const createCardForm = document.querySelector("#create-card-form")
+  createCardForm.addEventListener("submit", (e) => 
+    createFormHandler(e))
 })
 
 function getCards() {
   fetch(endPoint)
   .then(resp => resp.json())
   .then(cards => {
-    console.log(cards)
     cards.data.forEach(card => {
       const cardMarkup = `
         <div data-id=${card.id}>
@@ -22,4 +24,12 @@ function getCards() {
 
     })
   })
+}
+
+function createFormHandler(e) {
+  e.preventDefault()
+  const questionInput = document.querySelector('#input-question').value
+  const answerInput = document.querySelector('#input-answer').value
+  // const collectionId = document.querySelector("#")
+  postFetch(questionInput, answerInput)
 }
